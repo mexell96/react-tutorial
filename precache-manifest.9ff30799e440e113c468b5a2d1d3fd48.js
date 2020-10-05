3a38103c14c753d92ac27c2182e8c471cbf1ad83
Artem <mexexll96@gmail.com>
2020-10-05 15:24:23 +0300

    Updates

---
+++ precache-manifest.9ff30799e440e113c468b5a2d1d3fd48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5582784318e73fd48694a8082eda05",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "56897037df1702fe63b3",
+    "url": "/react-tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "f3a8912e9639c9ea3ccc",
+    "url": "/react-tutorial/static/js/2.1bbe048d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.1bbe048d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56897037df1702fe63b3",
+    "url": "/react-tutorial/static/js/main.76d2aa77.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
