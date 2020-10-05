957db133e9ed269511db14a27eac80594ee4cc18
Artem <mexexll96@gmail.com>
2020-10-05 16:00:05 +0300

    bug fix

--- src/Table.js
+++ src/Table.js
@@ -15,7 +15,7 @@ const TableBody = (props) => {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{index}</td>
+        <td>{index = index + 1}</td>
         <td>{row.name}</td>
         <td>{row.job}</td>
         <td>
